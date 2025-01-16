39786c8df1d27deebbc50d1abac5f5ad68f0a0f2
macsk <mac.skaanes@gmail.com>
2025-01-15 19:47:09 -0500

    UpdatedVolts Method

--- src/main/java/frc/robot/subsystems/tower/commands/setVolts.java
+++ src/main/java/frc/robot/subsystems/tower/commands/setVolts.java
@@ -24,12 +24,14 @@ public class setVolts extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    tower.setVolts(volts);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+    tower.setVolts(0.0);
   }
 
   // Returns true when the command should end.
